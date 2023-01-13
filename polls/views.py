35f8468e6f0899757158d23d22403c542bbf4867
tohara <tohara.220@gmail.com>
2023-01-13 15:56:14 +0900

    その3（ルーティング）が完了

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    '''HttpRequestクラスのrequestを受け取る'''
+    # 戻り値はHttpResponse形式になっている
+    return HttpResponse('Hello, World!!')
+
+def detail(request, question_id):
+    context = {'question_id': question_id}
+    return render(request, 'polls/detail.html', context)
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
